157623ac0cb2c233af4a6038ba67a338db78aef2
mpolevy <trusty@Johns-Mac-mini.local>
2017-02-23 06:36:51 -0500

    NR-16 NR-20 NR-21 NR-23 NR-27 Worked on scheduler page HTML/CSS

--- public/views/schedule/schedule-submission.controller.client.js
+++ public/views/schedule/schedule-submission.controller.client.js
@@ -6,8 +6,23 @@
     function ScheduleSubmissionController($routeParams, ClassService, ScheduleService) {
         var vm = this;
 
+        vm.deleteClass = deleteClass;
+
         function init() {
             vm.schedule = ScheduleService.getScheduleByTerm();
+        }
+
+        function deleteClass(classToDelete) {
+
+            var r = confirm("Are you sure you want to remove this class from this schedule?");
+            if (r == true) {
+                for (var x = 0; x < vm.schedule.length; x++) {
+                    if (vm.schedule[x].crn === classToDelete.crn) {
+                        vm.schedule.splice(x, 1);
+                        return;
+                    }
+                }
+            }
         }
 
         init();
