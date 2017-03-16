a8ce3fd734c8d845e24e5a178ffe601c8fd550a7
chenxun <chen.xun@husky.neu.edu>
2017-03-16 02:23:40 -0400

    various fixes on time & add peak period

--- public/views/class/class-detail.controller.client.js
+++ public/views/class/class-detail.controller.client.js
@@ -8,6 +8,9 @@
         var vm = this;
         vm.returnToSchedule = returnToSchedule;
         vm.saveAndReturnToSchedule = saveAndReturnToSchedule;
+        vm.isPeakPeriod = isPeakPeriod;
+        vm.updateEndingTimes = updateEndingTimes;
+        vm.updateOnChangeOfTime = updateOnChangeOfTime;
 
         function init() {
             if ($routeParams.crn === "_") {
@@ -37,21 +40,19 @@
             vm.allDoNotPublish = ClassService.getAllDoNotPublish();
             vm.allSpecialApprovals = ClassService.getAllSpecialApprovals();
 
-            timesOfDay = [];
+            //this will be replaced with data from previous semester
+            vm.allMeetingStartTimes = [];
             var count = 0;
-            for (var x = 0; x < 2400; x += 5) {
-                if (x % 100 == 60) {
-                    x += 35;
-                    continue;
+            for (var x = 6; x < 22; x += 1) {
+                for (var y = 0; y < 60; y += 5) {
+                    var hour = ("0" + x).slice(-2);
+                    var minute = ("0" + y).slice(-2);
+                    vm.allMeetingStartTimes[count] = hour + ":" + minute;
+                    count++;
                 }
-                var str = ("000" + x);
-                var str2 = str.slice(str.length - 4, str.length - 2) + ":" + str.slice(str.length - 2);
-                timesOfDay[count] = str2;
-                count++;
             }
 
-            vm.allMeetingStartTimes = timesOfDay;
-            vm.allMeetingEndTimes = timesOfDay;
+            vm.allMeetingEndTimes = updateEndingTimes();
         }
 
         function returnToSchedule() {
@@ -85,6 +86,38 @@
             oldClass.meetingDays = newClass.meetingDays;
             oldClass.meetingStart = newClass.meetingStart;
             oldClass.meetingEnd = newClass.meetingEnd;
+        }
+
+        function isPeakPeriod(time) {
+            if (vm.class.meetingDays == "M" || vm.class.meetingDays == "W" || vm.class.meetingDays == "R" ||
+                vm.class.meetingDays == "MW" || vm.class.meetingDays == "MWR") {
+                if ((time.slice(0, 2) == 15 && time.slice(-2) <= 25) ||
+                    (time.slice(0, 2) > 9 && time.slice(0, 2) < 15) ||
+                    (time.slice(0, 2) == 9 && time.slice(-2) >= 15)) {
+                    return 1;
+                }
+            }
+            if (vm.class.meetingDays == "T" || vm.class.meetingDays == "F" || vm.class.meetingDays == "TF") {
+                if ((time.slice(0, 2) == 15 && time.slice(-2) <= 25) ||
+                    (time.slice(0, 2) > 9 && time.slice(0, 2) < 15) ||
+                    (time.slice(0, 2) == 9 && time.slice(-2) >= 50)) {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        function updateEndingTimes() {
+            var startTimeIdx = vm.allMeetingStartTimes.indexOf(vm.class.meetingStart);
+            var classMinDuration = 30;
+            var classMaxDuration = 180;
+            vm.allMeetingEndTimes = vm.allMeetingStartTimes
+                .slice(startTimeIdx + classMinDuration / 5, startTimeIdx + classMaxDuration / 5);
+        }
+
+        function updateOnChangeOfTime() {
+            updateEndingTimes();
+            vm.isPeakPeriod = isPeakPeriod(vm.class.meetingStart) || isPeakPeriod(vm.class.meetingEnd);
         }
 
         init();
