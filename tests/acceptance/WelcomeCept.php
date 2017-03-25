fb49486b09a4936da44ec9bee4e58d87e3b9f625
Ubuntu <ubuntu@ip-172-31-24-186.us-west-2.compute.internal>
2017-03-25 17:01:04 +0000

    Added Codeception test files

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -1,6 +1,14 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+$I->wantTo('Test correct root page');
 $I->amOnPage('/');
 $I->see('Apache2 Ubuntu Default Page');
+
+$A = new AcceptanceTester($scenario);
+$A->wantTo('Test correct /public page');
+$A->amOnPage('/public/#/schedule-submission');
+$A->seeInCurrentUrl('public/#/schedule-submission');
+#$A->seeElement('myForm');
+$A->see('Schedule');
+#$A->seeElement(['css' => 'form'], ['name' => 'myForm']);
 ?>
