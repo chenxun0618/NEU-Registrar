230d608bb131143a7947a30a10eca8c88c1ce753
chenxun <cx584356321@gmail.com>
2017-03-26 23:46:00 -0400

    various fixes and code quality enhancements

--- connection/Utils.php
+++ connection/Utils.php
@@ -1,40 +1,67 @@
 <?php
+/*
+ * a utility class that contains all the framework functions
+ */
 class Utils
 {
-    protected static $conn;
+    protected $conn;
 
-    function connect()
+    /*
+     * set up the connection to the database
+     */
+    function __construct()
     {
         // If a connection has not been established yet, establish it
-        if (!isset(self::$conn)) {
+        if (!isset($this->conn)) {
             // Load configuration
             $config = parse_ini_file('../config.ini');
-            self::$conn = new mysqli('localhost', $config['username'], $config['password'], $config['database']);
+            $this->conn = new mysqli($config['hostname'], $config['username'], $config['password'], $config['database']);
         }
 
         // If connection was not established
-        if (!self::$conn)
-            die("Connection failed: " . mysqli_connect_error());
-
-        return self::$conn;
+        if (!$this->conn)
+            die("Connection failed: " . $this->conn->connect_error);
     }
 
+    /*
+     * connect to the database and construct a query
+     */
     function query($query)
     {
-        // Connect to the database
-        $conn = $this->connect();
-
         // Query the database
-        $result = $conn->query($query);
+        $result = $this->conn->query($query);
 
         // If database cannot process the query
-        if (!$result)
-            die("Couldn't find the information: " . mysqli_error($conn));
+        if (!$result) {
+            $this->conn->close();
+            die("Couldn't find the information: " . $this->conn->error);
+        }
+
+        // If there is no rows in the result
+        if (mysqli_num_rows($result) <= 0) {
+            $this->conn->close();
+            die("0 results");
+        }
 
         return $result;
     }
+
+    /*
+     * get the value from front-end request
+     */
+    function get($val)
+    {
+        if (!isset($_GET[$val])) {
+            die($val . "is not valid");
+        }
+        return $_GET[$val];
+    }
+
+    /*
+     * close the connection to the database
+     */
+    function __destruct()
+    {
+        $this->conn->close();
+    }
 }
-
-
-
-
