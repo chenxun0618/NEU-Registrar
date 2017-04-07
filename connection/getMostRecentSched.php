32f4a5e6c341b885326f6b580484bc891c4a03b3
chenxun <chen.xun@husky.neu.edu>
2017-04-07 00:35:08 -0400

    small fixes

--- connection/getMostRecentSched.php
+++ connection/getMostRecentSched.php
@@ -4,12 +4,13 @@ include 'utils.php';
 $utils = new Utils();
 $dept = $utils->get('dept');
 $query = "CALL getMostRecentSched('$dept')";
-$schedule = $utils->query($query);
+$result = $utils->query($query);
+$schedule = $result[0];
 
 if (array_key_exists("classes", $schedule)) {
     echo json_encode($schedule);
 } else {
     $classes = $utils->query("CALL getClassesByDept('$dept')");
-    array_push($schedule, $classes);
+    $schedule["classes"] = $classes;
     echo json_encode($schedule);
 }
