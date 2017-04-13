d336c3a9e6916878860b8fad71a60a32cdade279
mdang8 <mdang808@gmail.com>
2017-04-13 11:15:12 -0400

    Reformatted code, added more comments, and fixed variable names

--- lib/courseCatalogLookup.php
+++ lib/courseCatalogLookup.php
@@ -1,22 +1,23 @@
 <?php
-    include 'RegistrarDatabase.php';
+// @codeCoverageIgnoreStart
+include 'RegistrarDatabase.php';
 
-    if (isset($_GET['subjectCode'])) {
-        $subjectCode = $_GET['subjectCode'];
-    } else {
-        echo "Error: Subject code parameter was not set.";
-        return;
-    }
+if (isset($_GET['subjectCode'])) {
+    $subjectCode = $_GET['subjectCode'];
+} else {
+    echo "Error: Subject code parameter was not set.";
+    return;
+}
 
-    if (isset($_GET['courseNumber'])) {
-        $courseNumber = $_GET['courseNumber'];
-    } else {
-        echo "Error: Course number parameter was not set.";
-        return;
-    }
+if (isset($_GET['courseNumber'])) {
+    $courseNumber = $_GET['courseNumber'];
+} else {
+    echo "Error: Course number parameter was not set.";
+    return;
+}
 
-    $db = new RegistrarDatabase();
-    $courseCatalog = $db->getCourseCatalog($subjectCode, $courseNumber)[0];
+$db = new RegistrarDatabase();
+$courseCatalog = $db->getCourseCatalog($subjectCode, $courseNumber)[0];
 
-    echo json_encode($courseCatalog);
-?>
+echo json_encode($courseCatalog);
+// @codeCoverageIgnoreEnd
