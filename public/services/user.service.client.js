8a04d4e1dd3548cb369f1613e65b7514e969ab82
mpolevy <trusty@Johns-Mac-mini.local>
2017-04-12 22:33:49 -0400

    NR-66 added login

--- public/services/user.service.client.js
+++ public/services/user.service.client.js
@@ -3,35 +3,15 @@
         .module("NEURegistrar")
         .factory("UserService", UserService);
 
-    function UserService() {
+    function UserService($http) {
 
         var api = {
             login: login
         };
 
-        var dictionary = [
-            {
-                nuid: "000000000",
-                email: "n.rakitin@northeastern.edu",
-                departments: [],
-                admin: true
-            },
-            {
-                nuid: "111111111",
-                email: "polevy.m@husky.neu.edu",
-                departments: ["CS", "IS"],
-                admin: false
-            }
-        ];
-
         function login(email, nuid) {
-            for (var x = 0; x < dictionary.length; x++) {
-                var user = dictionary[x];
-                if (user.nuid === nuid && user.email === email) {
-                    return user;
-                }
-            }
-            return null;
+            var url = "/connection/login.php?NUID=" + nuid + "&email=" + encodeURIComponent(email);
+            return $http.get(url);
         }
 
         return api;
--- public/views/user/login.controller.client.js
+++ public/views/user/login.controller.client.js
@@ -15,15 +15,42 @@
         vm.login = login;
 
         function login(email, nuid) {
-            var user = UserService.login(email, nuid);
+            var user;
+            UserService.login(email, nuid)
+                .then(
+                    function (res) {
+                        user = res.data;
+                        reformatSchedule(user);
+                        vm.error = "";
+                        $window.sessionStorage.loggedInUser = JSON.stringify(user);
+                        $location.url("/schedule-submission/");
+                    },
+                    function (error) {
+                        vm.error = error.statusText;
+                    }
+                );
+        };
 
-            if (user) {
-                vm.error = "";
-                $window.sessionStorage.loggedInUser = JSON.stringify(user);
-                $location.url("/schedule-submission/");
-            } else {
-                vm.error = "Email / NUID pair not found";
+        function reformatSchedule(user) {
+            var newDepts = [];
+
+            var depts = user.dept;
+            for (var x = 0; x < depts.length; x++) {
+                var dept = depts[x];
+                var deptName = Object.keys(dept)[0];
+                var scheduleStatus = dept[deptName];
+                newDepts.push({
+                    code: deptName,
+                    scheduleStatus: scheduleStatus
+                });
             }
+
+            user.depts = newDepts;
+            user.admin = parseInt(user.isAdmin) ? true : false;
+            user.nuid = user.NUID;
+            delete user.dept;
+            delete user.isAdmin;
+            delete user.NUID;
         }
     }
 })();
