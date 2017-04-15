4d8f15c3991e3627d36759da5e7c04d0d2845894
mdang8 <mdang808@gmail.com>
2017-04-14 21:35:06 -0400

    Fixed config file path and formatting

--- lib/DB.php
+++ lib/DB.php
@@ -4,21 +4,19 @@
  * a utility class that contains all the framework functions
  */
 
-class DB
-{
+class DB {
     protected $conn;
 
     /*
      * set up the connection to the database
      */
-    function __construct()
-    {
+    function __construct() {
         // If a connection has not been established yet, establish it
         if (!isset($this->conn)) {
             // Load configuration
             $config = parse_ini_file('config/config.ini');
             $this->conn = new mysqli($config['hostname'], $config['username'],
-                $config['password'], $config['database']);
+                    $config['password'], $config['database']);
         }
 
         // If connection was not established
@@ -33,8 +31,7 @@ class DB
     /*
      * close the connection to the database
      */
-    function __destruct()
-    {
+    function __destruct() {
         $this->conn->close();
     }
 
@@ -42,8 +39,7 @@ class DB
      * get the value from front-end get request
      */
     // @codeCoverageIgnoreStart
-    function get($val)
-    {
+    function get($val) {
         if (!isset($_GET[$val])) {
             $this->header(400, $val . " is not valid");
             die;
@@ -54,8 +50,7 @@ class DB
     /*
      * get the value from front-end post request
      */
-    function post($val)
-    {
+    function post($val) {
         if (!isset($_POST[$val])) {
             $this->header(400, $val . " is not valid");
             die;
@@ -66,8 +61,7 @@ class DB
     /*
      * set the response header
      */
-    function header($code, $text)
-    {
+    function header($code, $text) {
         header("HTTP/1.1 " . $code . ' ' . $text);
     }
     // @codeCoverageIgnoreEnd
@@ -75,8 +69,7 @@ class DB
     /*
      * perform a query on the database and return the retrieved data in an array
      */
-    function query($query)
-    {
+    function query($query) {
         // Query the database
         $result = $this->conn->query($query);
 
@@ -118,8 +111,7 @@ class DB
      * set the header of the response and return json_encoded result
      */
     // @codeCoverageIgnoreStart
-    function return_json($code, $json)
-    {
+    function return_json($code, $json) {
         if ($code / 100 == 2)
             $text = "OK";
         else
